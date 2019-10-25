143889514cec8e058bfb790e4474f4b3ddb8708b
keitami10 <57019635+keitami10@users.noreply.github.com>
2019-10-25 18:55:30 -0300

    archivos para no perder

---
+++ index.js
@@ -0,0 +1,24 @@
+
+var salamandra=true;
+var  mensaje;
+var semana = ["lunes","martes","miercoles","jueves","viernes"];
+for(var i=0 ; i<7 ; i++){
+    alert(semana[i]);
+}
+if(salamandra){
+
+    alert("Bienvenido a mi sitio xdxd");
+} else{ 
+    mensaje='seguir durmiendo'
+}
+alert(mensaje)
+prompt('Que edad     tenes?');
+
+if (edad>= 12){
+    mensaje = 'Bienvenido a mi sitio';
+} else if (edad > 10 && edad < 12){
+    mensaje='Bueno, quedate si quieres';
+}else {
+    mensaje='chau!';
+}
+alert(mesaje);
